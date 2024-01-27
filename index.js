4fd3efe0be56b5c040a8bc25d87cec4570052f16
nirtendra <nirtendrayadav12@gmail.com>
2024-01-27 19:14:43 +1100

    completed labs array find

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+
+
+function superbowlWin(array) {
+    const winningLetter = array.find(element => element.result ==="W")
+    if (winningLetter){
+        return winningLetter.year;
+
+    }else{
+        return undefined;
+    }
+    
+   
+}
